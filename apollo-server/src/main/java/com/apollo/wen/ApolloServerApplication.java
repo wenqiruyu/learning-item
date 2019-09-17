25097c6d839de5c97c37eb46d251e21b12091d14
wenqiruyu <2234150293@qq.com>
2019-09-18 00:49:05 +0800

    提交代码

---
+++ apollo-server/src/main/java/com/apollo/wen/ApolloServerApplication.java
@@ -0,0 +1,13 @@
+package com.apollo.wen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApolloServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApolloServerApplication.class, args);
+    }
+
+}
---
+++ eureka-server/src/main/java/com/eureka/wen/config/WebSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.eureka.wen.config;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ *  eureka开启验证后无法连接注册中心?
+ *  spring Cloud 2.0 以上）的security默认启用了csrf检验，要在eurekaServer端配置security的csrf检验为false
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        super.configure(http);
+    }
+}
